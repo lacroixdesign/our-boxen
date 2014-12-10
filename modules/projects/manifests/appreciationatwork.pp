48b81e584c15b53addc11c6cd88e24603145722c
Michael LaCroix <michael@lacroixdesign.net>
2014-12-10 15:48:18 -0600

    Add appreciationatwork.com project

---
+++ modules/projects/manifests/appreciationatwork.pp
@@ -0,0 +1,17 @@
+class projects::appreciationatwork {
+  require config::php
+
+  $local_name = 'appreciationatwork'
+
+  php::project { $local_name:
+    source        => 'git@bitbucket.org:lacroixdesign/appreciation-at-work.git',
+    dir           => "${boxen::config::srcdir}/${local_name}.com",
+    # Common defaults
+    server_name   => "${local_name}.dev",
+    mysql         => "${local_name}_dev",
+    dotenv        => true,
+    # Stop editing
+    nginx         => 'projects/shared/wordpress.conf.erb',
+    php           => $config::php::default_php,
+  }
+}
