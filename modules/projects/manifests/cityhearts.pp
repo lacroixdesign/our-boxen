616a90523ab4b136f3c31baec7cfecbb0483a20a
Michael LaCroix <michael@lacroixdesign.net>
2015-01-25 15:34:15 -0600

    Add City Hearts project

---
+++ modules/projects/manifests/cityhearts.pp
@@ -0,0 +1,16 @@
+class projects::cityhearts {
+  require config::php
+
+  $local_name = 'cityheartschicago'
+
+  php::project { $local_name:
+    source        => 'git@bitbucket.org:lacroixdesign/city-hearts.git',
+    dir           => "${boxen::config::srcdir}/${local_name}.org",
+    # Common defaults
+    server_name   => "${local_name}.craft.dev",
+    mysql         => "${local_name}_dev",
+    # Stop editing
+    nginx         => 'projects/shared/craft.conf.erb',
+    php           => $config::php::default_php,
+  }
+}
