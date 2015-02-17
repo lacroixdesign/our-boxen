3afd8f70ff488c1aafbde69663f42b3f3ce8a65d
Michael LaCroix <michael@lacroixdesign.net>
2015-02-17 17:59:06 -0600

    Add Craft boilerplate projects

---
+++ modules/projects/manifests/craft-gulp-boilerplate.pp
@@ -0,0 +1,15 @@
+class projects::craft-gulp-boilerplate {
+  require config::php
+
+  $local_name = 'craft-gulp-boilerplate'
+
+  php::project { $local_name:
+    source        => "git@github.com:iamlacroix/${local_name}.git",
+    dir           => "${boxen::config::srcdir}/_templates_/${local_name}",
+    # Common defaults
+    server_name   => "boilerplate-gulp.craft.dev",
+    # Stop editing
+    nginx         => 'projects/shared/craft.conf.erb',
+    php           => $config::php::default_php,
+  }
+}
---
+++ modules/projects/manifests/craft-webpack-boilerplate.pp
@@ -0,0 +1,16 @@
+class projects::craft-webpack-boilerplate {
+  require config::php
+
+  $local_name = 'craft-webpack-boilerplate'
+
+  php::project { $local_name:
+    source        => "git@github.com:iamlacroix/${local_name}.git",
+    dir           => "${boxen::config::srcdir}/_templates_/${local_name}",
+    # Common defaults
+    server_name   => "boilerplate-webpack.craft.dev",
+    mysql         => "craftboilerplate_dev",
+    # Stop editing
+    nginx         => 'projects/shared/craft.conf.erb',
+    php           => $config::php::default_php,
+  }
+}
