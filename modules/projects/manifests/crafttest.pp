e846a1a00b18d56342fdb7a5c59dd72729977e66
Michael LaCroix <michael@lacroixdesign.net>
2015-03-10 12:04:04 -0500

    Add Craft CMS test project

---
+++ modules/projects/manifests/crafttest.pp
@@ -0,0 +1,16 @@
+class projects::crafttest {
+  require config::php
+
+  $local_name = 'crafttest'
+
+  php::project { $local_name:
+    source        => 'git@github.com:iamlacroix/craft-gulp-boilerplate.git',
+    dir           => "${boxen::config::srcdir}/_tools_/test.craft.dev",
+    # Common defaults
+    server_name   => "test.craft.dev",
+    mysql         => "craft_test",
+    # Stop editing
+    nginx         => 'projects/shared/craft.conf.erb',
+    php           => $config::php::default_php,
+  }
+}
