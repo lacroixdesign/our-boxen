632d4c630b6cdfe290b31265797a8ca628ebd5fe
Michael LaCroix <michael@lacroixdesign.net>
2015-09-24 17:15:34 -0500

    Update Node version

--- modules/config/manifests/node.pp
+++ modules/config/manifests/node.pp
@@ -1,63 +1,30 @@
 class config::node {
   include nodejs
 
-  $node_version = 'iojs-2.4.0'
+  $v = '4.0.0'
 
   class { 'nodejs::global':
-    version => $node_version
+    version => $v
   }
 
   nodejs::version { '0.12.7': }
 
   # npm modules
 
-  npm_module { "bower for ${node_version}":
-    module => 'bower',
-    node_version => $node_version
-  }
-  npm_module { "gulp for ${node_version}":
-    module => 'gulp',
-    node_version => $node_version
-  }
-  npm_module { "eslint for ${node_version}":
-    module => 'eslint',
-    node_version => $node_version
-  }
-  npm_module { "eslint-plugin-babel for ${node_version}":
-    module => 'eslint-plugin-babel',
-    node_version => $node_version
-  }
-  npm_module { "eslint-plugin-react for ${node_version}":
-    module => 'eslint-plugin-react',
-    node_version => $node_version
-  }
-  npm_module { "babel-eslint for ${node_version}":
-    module => 'babel-eslint',
-    node_version => $node_version
-  }
-  # npm_module { "browser-sync for ${node_version}":
-  #   module => 'browser-sync',
-  #   node_version => $node_version
-  # }
-  npm_module { "jshint for ${node_version}":
-    module => 'jshint',
-    node_version => $node_version
-  }
-  npm_module { "uglify-js for ${node_version}":
-    module => 'uglify-js',
-    node_version => $node_version
-  }
-  npm_module { "grunt-cli for ${node_version}":
-    module => 'grunt-cli',
-    node_version => $node_version
-  }
-  # npm_module { "weinre for ${node_version}":
-  #   module => 'weinre',
-  #   node_version => $node_version
-  # }
-  npm_module { "svgo for ${node_version}":
-    module => 'svgo',
-    node_version => $node_version
-  }
+  npm_module { "babel for Node ${v}": module => 'babel', node_version => $v }
+  npm_module { "babel-eslint for Node ${v}": module => 'babel-eslint', node_version => $v }
+  npm_module { "bower for Node ${v}": module => 'bower', node_version => $v }
+  npm_module { "datauri for Node ${v}": module => 'datauri', node_version => $v }
+  npm_module { "ember-cli for Node ${v}": module => 'ember-cli', node_version => $v }
+  npm_module { "eslint for Node ${v}": module => 'eslint', node_version => $v }
+  npm_module { "eslint-plugin-babel for Node ${v}": module => 'eslint-plugin-babel', node_version => $v }
+  npm_module { "eslint-plugin-react for Node ${v}": module => 'eslint-plugin-react', node_version => $v }
+  npm_module { "gulp for Node ${v}": module => 'gulp', node_version => $v }
+  npm_module { "grunt-cli for Node ${v}": module => 'grunt-cli', node_version => $v }
+  npm_module { "jshint for Node ${v}": module => 'jshint', node_version => $v }
+  npm_module { "nodemon for Node ${v}": module => 'nodemon', node_version => $v }
+  npm_module { "svgo for Node ${v}": module => 'svgo', node_version => $v }
+  npm_module { "uglify-js for Node ${v}": module => 'uglify-js', node_version => $v }
+  npm_module { "yo for Node ${v}": module => 'yo', node_version => $v }
 
 }
