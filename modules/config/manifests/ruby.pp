587206cdc74b38eb7460129e454d6eb5257ac1ec
Michael LaCroix <michael@lacroixdesign.net>
2015-12-09 11:04:00 -0600

    Remove old versions of Ruby

--- modules/config/manifests/ruby.pp
+++ modules/config/manifests/ruby.pp
@@ -2,8 +2,8 @@ class config::ruby {
   $rb_version = "2.2.0"
   ruby::version { $rb_version: }
   ruby::version { '2.1.5': }
-  ruby::version { '2.0.0': }
-  ruby::version { '1.9.3': }
+  # ruby::version { '2.0.0': }
+  # ruby::version { '1.9.3': }
 
   ruby_gem { "bundler for ${rb_version}":
     gem          => 'bundler',
