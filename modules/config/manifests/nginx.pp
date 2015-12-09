dc3f1cbccaadda704f5d4982d83f8c650a5a4150
Michael LaCroix <michael@lacroixdesign.net>
2015-12-08 19:29:28 -0600

    Add basic Docker nginx proxy

--- modules/config/manifests/nginx.pp
+++ modules/config/manifests/nginx.pp
@@ -1,12 +1,22 @@
 class config::nginx {
   include nginx
 
-  $config_dir  = $nginx::config::sitesdir
-  $template    = 'config/forward.conf'
-  $config_file = "${config_dir}/_dev_forwarding.conf"
+  $config_dir = $nginx::config::sitesdir
 
-  file { $config_file:
-    content => template($template),
+  $fwd_template = 'config/forward.conf'
+  $fwd_config   = "${config_dir}/_dev_forwarding.conf"
+
+  $docker_template = 'config/docker.conf'
+  $docker_config   = "${config_dir}/_dev_docker.conf"
+
+  file { $fwd_config:
+    content => template($fwd_template),
+    require => File[$config_dir],
+    notify  => Service['dev.nginx'],
+  }
+
+  file { $docker_config:
+    content => template($docker_template),
     require => File[$config_dir],
     notify  => Service['dev.nginx'],
   }
