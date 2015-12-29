d9e362fd6cb42fa7f641d8de1290d050ba4bd55b
Michael LaCroix <michael@lacroixdesign.net>
2015-12-29 15:01:31 -0600

    Use new Dropbox folder name

--- modules/people/manifests/iamlacroix/config.pp
+++ modules/people/manifests/iamlacroix/config.pp
@@ -1,7 +1,7 @@
 class people::iamlacroix::config {
   $home         = "/Users/${::boxen_user}"
   $dotfiles_dir = "${boxen::config::srcdir}/dotfiles"
-  $dropbox_dir  = "${home}/Dropbox"
+  $dropbox_dir  = "${home}/Dropbox (Personal)"
   $googledrive_dir  = "${home}/Google Drive"
   $dropbox_sync_dir = "${dropbox_dir}/Sync"
   $gdrive_sync_dir  = "${googledrive_dir}/Sync"
--- modules/people/manifests/jamesdlacroix/config.pp
+++ modules/people/manifests/jamesdlacroix/config.pp
@@ -1,7 +1,7 @@
 class people::jamesdlacroix::config {
   $home         = "/Users/${::boxen_user}"
   $dotfiles_dir = "${boxen::config::srcdir}/dotfiles"
-  $dropbox_dir  = "${home}/Dropbox"
+  $dropbox_dir  = "${home}/Dropbox (Personal)"
   $dropbox_sync_dir     = "${dropbox_dir}/Sync"
 
   file { [$dropbox_dir, $dropbox_sync_dir]:
